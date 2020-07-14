faed6aaaded212f1afab88b179e3de701a979c63
Leonlevitan <68025638+Leonlevitan@users.noreply.github.com>
2020-07-13 23:55:40 -0300

    Add files via upload
    commit 1

--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -3,6 +3,12 @@ Debemos lograr tomar un nombre con 'prompt'
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	alert("ok");
-}
+	let nombre; //declaracion de variable//
+
+	nombre = prompt("Ingrese su nombre"); // se ejecura de derecha a izquierda
+
+	//nombre = "leon"; //le digo que nombre es igual a texto literal leon
+
+
+	alert(nombre);}
 
